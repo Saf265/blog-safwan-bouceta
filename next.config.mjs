35c5b522a7673b62257257ea5f4548f62fec36ee
Saf2339 <boucetasafwan@gmail.com>
2024-10-11 23:56:34 +0200

    Problem Next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Appliquez ces en-têtes à toutes les routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(self), microphone=(), camera=()",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
