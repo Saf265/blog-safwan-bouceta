ab71f62109511b6e8cb5ff25f9ae1fdcfc76d73d
Saf2339 <boucetasafwan@gmail.com>
2024-10-11 18:34:17 +0200

    First Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <header className="navbar my-4 flex items-center justify-between rounded-3xl border px-3 shadow-xl">
+      <div className="flex-1">
+        <a href="/" className="btn btn-ghost text-xl">
+          Safwan Bouceta
+        </a>
+      </div>
+      <div className="mr-2 flex-none">
+        <Link href="/" className="link link-primary font-semibold">
+          Posts
+        </Link>
+      </div>
+    </header>
+  );
+};
