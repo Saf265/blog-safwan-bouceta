1bcd0f2c18da3e97b0ceeb1820ce295b03f97c8c
Saf2339 <boucetasafwan@gmail.com>
2024-10-12 10:20:07 +0200

    Problem Next v3

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,6 +1,27 @@
 import { Mdx } from "@/features/mdx/Mdx";
 import { getPost } from "@/lib/posts";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+
+export const dynamic = "force-static";
+
+export const generateMetadata = async (props: {
+  params: { slug: string };
+}): Promise<Metadata> => {
+  const post = await getPost(props.params.slug);
+
+  if (!post) {
+    return {
+      title: "404 - Page non trouvée",
+      description: "Page non trouvée",
+    };
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+  };
+};
 
 export default async function RoutePage(props: { params: { slug: string } }) {
   const post = await getPost(props.params.slug);
